b2de9f4c247677f0011418c9e1bd0c9b9812eb2f
Alexander Pozakshin <darkman.russia@gmail.com>
2025-11-08 22:49:27 +0300

    Initial commit for SwiftUI.Example

---
+++ SwiftUI.Example/SwiftUI.Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI.Example
+//
+//  Created by Alexander Pozakshin on 08.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
